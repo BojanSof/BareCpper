d7f2c7944e0e476e9c2efb1660d87dcae735182b
Bojan Sofronievski <bojan.drago@gmail.com>
2022-09-14 16:08:28 +0200

    Small changes, sync transfer still not working properly.

--- include/BareCpper/ATsamd/SpiAsync_ATsamd5x.hpp
+++ include/BareCpper/ATsamd/SpiAsync_ATsamd5x.hpp
@@ -90,10 +90,10 @@ namespace BareCpper
 
         platformConfig_ = platformConfig;
         pins_ = pins;
-        return initialiseClock(*sercomIndex, platformConfig)
+        return initialiseClock()
               && initialiseGpio()
-              && initialiseDevice(*sercomIndex, pins)
-              && initialiseAsync(*sercomIndex)
+              && initialiseDevice()
+              && initialiseAsync()
             ;
       }
 
@@ -110,7 +110,6 @@ namespace BareCpper
 
         hw_->CTRLA.reg = (hw_->CTRLA.reg & ~ctrlAMask) | ctrlA;
         hw_->BAUD.bit.BAUD = baudBit;
-        hw_->CTRLC.bit.DATA32B = platformConfig_.use32BitExtension;
         // SERCOM_CRITICAL_SECTION_LEAVE();
 
         return true;
@@ -120,8 +119,7 @@ namespace BareCpper
       {
         hw_->CTRLA.bit.ENABLE = false;
         hw_->SYNCBUSY.bit.SWRST = true;
-        while (hw_->SYNCBUSY.bit.SWRST | hw_->SYNCBUSY.bit.ENABLE)
-          ;
+        while (hw_->SYNCBUSY.bit.SWRST | hw_->SYNCBUSY.bit.ENABLE);
         return true;
       }
 
@@ -145,8 +143,8 @@ namespace BareCpper
        */
       int32_t asyncTransfer(const SpiMessage<DataType> &message)
       {
-        static size_t iTxBuffer = 0;
-        static size_t iRxBuffer = 0;
+        static volatile size_t iTxBuffer = 0;
+        static volatile size_t iRxBuffer = 0;
 
         if (transferInProgress_)
           return -1;
@@ -158,30 +156,27 @@ namespace BareCpper
         {
           BareCpper::sercomTxCallbacks[sercomIndex_] = [this, message]()
           {
-            if (hw_->INTFLAG.reg & SERCOM_SPI_INTFLAG_TXC) ///@todo: not needed, Handler is dedicated to TXC IRQ
+            // if all bytes are transferred, disable TXC IRQ
+            // check if RXC IRQ is disabled and stop SPI clock if that is the case
+            ///@todo race condition on INTENSET_RXC
+            if (iTxBuffer == message.bufferLength)
             {
-              // if all bytes are transferred, disable TXC IRQ
-              // check if RXC IRQ is disabled and stop SPI clock if that is the case
-              ///@todo race condition on INTENSET_RXC
-              if (iTxBuffer == message.bufferLength)
+              hw_->INTENCLR.reg |= SERCOM_SPI_INTENCLR_TXC;
+              hw_->INTFLAG.reg |= SERCOM_SPI_INTFLAG_TXC;  //< clear TXC flag
+              if (!(hw_->INTENSET.reg & SERCOM_SPI_INTENSET_RXC))
               {
-                hw_->INTENCLR.reg |= SERCOM_SPI_INTENCLR_TXC;
-                hw_->INTFLAG.reg |= SERCOM_SPI_INTFLAG_TXC;  //< clear TXC flag
-                if (!(hw_->INTENSET.reg & SERCOM_SPI_INTENSET_RXC))
-                {
-                  BareCpper::gpioOutHigh(pins_.cs);
-                  disableSync();
-                  transferInProgress_ = false;
-                }
+                BareCpper::gpioOutHigh(pins_.cs);
+                disableSync();
+                transferInProgress_ = false;
               }
-              else
-              {
-                // transfer new data, also clears TXC irq flag
-                hw_->DATA.reg = message.txBuffer[iTxBuffer++];
-              }
-              // call user callback
-              if(message.txCallback) message.txCallback();
             }
+            else
+            {
+              // transfer new data, also clears TXC irq flag
+              hw_->DATA.reg = message.txBuffer[iTxBuffer++];
+            }
+            // call user callback
+            if(message.txCallback) message.txCallback();
           };
           // enable TXC IRQ
           hw_->INTENSET.reg |= SERCOM_SPI_INTENSET_TXC;
@@ -191,33 +186,31 @@ namespace BareCpper
         {
           BareCpper::sercomRxCallbacks[sercomIndex_] = [this, message]()
           {
-            if (hw_->INTFLAG.reg & SERCOM_SPI_INTFLAG_RXC)  ///@todo: not needed, Handler is dedicated to RXC IRQ
+            if (iRxBuffer < message.bufferLength)
             {
-              if (iRxBuffer < message.bufferLength)
-              {
-                // read received data, also clears RXC irq flag
-                message.rxBuffer[iRxBuffer++] = hw_->DATA.reg;
-              }
-              // if all bytes are received, disable RXC IRQ
-              // check if TXC IRQ is disabled and stop SPI clock if that is the case
-              ///@todo race condition on INTENSET_TXC
-              else
-              {
-                hw_->INTENCLR.reg |= SERCOM_SPI_INTENCLR_RXC;
-                if (!(hw_->INTENSET.reg & SERCOM_SPI_INTENSET_TXC))
-                {
-                  BareCpper::gpioOutHigh(pins_.cs);
-                  disableSync();
-                  transferInProgress_ = false;
-                }
-              }
-              // call user callback
-              if(message.rxCallback) message.rxCallback();
+              // read received data, also clears RXC irq flag
+              message.rxBuffer[iRxBuffer++] = hw_->DATA.reg;
             }
+            // if all bytes are received, disable RXC IRQ
+            // check if TXC IRQ is disabled and stop SPI clock if that is the case
+            ///@todo race condition on INTENSET_TXC
+            else
+            {
+              hw_->INTENCLR.reg |= SERCOM_SPI_INTENCLR_RXC;
+              if (!(hw_->INTENSET.reg & SERCOM_SPI_INTENSET_TXC))
+              {
+                BareCpper::gpioOutHigh(pins_.cs);
+                disableSync();
+                transferInProgress_ = false;
+              }
+            }
+            // call user callback
+            if(message.rxCallback) message.rxCallback();
           };
           // enable RXC IRQ
           hw_->INTENSET.reg |= SERCOM_SPI_INTENSET_RXC;
         }
+        
         // enable SPI clock
         enableSync();
         // pull CS low
@@ -233,29 +226,31 @@ namespace BareCpper
         return 1;
       }
 
-      int32_t syncTransfer(SpiMessage<DataType> &message)
+      int32_t syncTransfer(const SpiMessage<DataType>& message)
       {
         if (transferInProgress_)
           return -1;
 
-        volatile size_t iTxBuffer = 0, iRxBuffer = 0;
-        message.txCallback = [&iTxBuffer]() { ++iTxBuffer; };
-        message.rxCallback = [&iRxBuffer]() { ++iRxBuffer; };
+        // volatile size_t iTxBuffer = 0, iRxBuffer = 0;
+        // message.txCallback = [&iTxBuffer]() { ++iTxBuffer; };
+        // message.rxCallback = [&iRxBuffer]() { ++iRxBuffer; };
 
         if(asyncTransfer(message) < 0) return -1;
 
-        if(message.txBuffer == nullptr)
-        {
-          while((iRxBuffer < message.bufferLength));
-        }
-        else if(message.rxBuffer == nullptr)
-        {
-          while((iTxBuffer < message.bufferLength));
-        }
-        else
-        {
-          while((iTxBuffer < message.bufferLength) || (iRxBuffer < message.bufferLength));  //< wait for transfer to finish          
-        }
+        while(transferInProgress_);
+
+        // if(message.txBuffer == nullptr)
+        // {
+        //   while((iRxBuffer < message.bufferLength));
+        // }
+        // else if(message.rxBuffer == nullptr)
+        // {
+        //   while((iTxBuffer < message.bufferLength));
+        // }
+        // else
+        // {
+        //   while((iTxBuffer < message.bufferLength) || (iRxBuffer < message.bufferLength));  //< wait for transfer to finish          
+        // }
 
         return 1;
       }
@@ -294,11 +289,11 @@ namespace BareCpper
         return true;
       }
 
-      bool initialiseClock(const uint8_t sercomIndex, const platformConfig_t &platformConfig)
+      bool initialiseClock()
       {
         /// @TODO Power-Saving: TBC if Slow-Clock even necessary
 
-        const ATsamd5x::SercomClocks params = ATsamd5x::sercomClocks(sercomIndex);
+        const ATsamd5x::SercomClocks params = ATsamd5x::sercomClocks(sercomIndex_);
 
         GCLK->PCHCTRL[params.gclkId_CORE].bit.CHEN = 0; // Disable timer
         GCLK->PCHCTRL[params.gclkId_SLOW].bit.CHEN = 0; // Disable timer
@@ -306,12 +301,12 @@ namespace BareCpper
         while (GCLK->PCHCTRL[params.gclkId_SLOW].bit.CHEN || GCLK->PCHCTRL[params.gclkId_CORE].bit.CHEN); // Wait for disable
 
         // GCLK_CRITICAL_SECTION_ENTER();
-        GCLK->PCHCTRL[params.gclkId_CORE].reg = platformConfig.gclkId_CORE_Src | GCLK_PCHCTRL_CHEN;
-        GCLK->PCHCTRL[params.gclkId_SLOW].reg = platformConfig.gclkId_SLOW_Src | GCLK_PCHCTRL_CHEN;
+        GCLK->PCHCTRL[params.gclkId_CORE].reg = platformConfig_.gclkId_CORE_Src | GCLK_PCHCTRL_CHEN;
+        GCLK->PCHCTRL[params.gclkId_SLOW].reg = platformConfig_.gclkId_SLOW_Src | GCLK_PCHCTRL_CHEN;
         // GCLK_CRITICAL_SECTION_LEAVE();
 
         // MCLK_CRITICAL_SECTION_ENTER();
-        ATsamd5x::sercomApbEnable(sercomIndex);
+        ATsamd5x::sercomApbEnable(sercomIndex_);
         // MCLK_CRITICAL_SECTION_LEAVE();
 
         while (!GCLK->PCHCTRL[params.gclkId_CORE].bit.CHEN && !GCLK->PCHCTRL[params.gclkId_SLOW].bit.CHEN); // Wait for clock enable
@@ -319,9 +314,9 @@ namespace BareCpper
         return true;
       }
 
-      bool initialiseDevice(const uint8_t sercomIndex, const Pins_t &pins)
+      bool initialiseDevice()
       {
-        ::Sercom *sercom = ATsamd5x::sercom(sercomIndex);
+        ::Sercom *sercom = ATsamd5x::sercom(sercomIndex_);
         if (!sercom)
           return false;
         hw_ = &sercom->SPI;
@@ -339,9 +334,9 @@ namespace BareCpper
 
         while (hw_->SYNCBUSY.bit.SWRST); // Wait for reset
 
-        const uint8_t mosiPad = *ATsamd5x::sercomPinPad(sercomIndex, pins.mosi); //<@note We know the result shall be valid derefernece as this is checked via sercomForPins()
-        const uint8_t misoPad = *ATsamd5x::sercomPinPad(sercomIndex, pins.miso);
-        const uint8_t sckPad = *ATsamd5x::sercomPinPad(sercomIndex, pins.sck);
+        const uint8_t mosiPad = *ATsamd5x::sercomPinPad(sercomIndex_, pins_.mosi); //<@note We know the result shall be valid derefernece as this is checked via sercomForPins()
+        const uint8_t misoPad = *ATsamd5x::sercomPinPad(sercomIndex_, pins_.miso);
+        const uint8_t sckPad = *ATsamd5x::sercomPinPad(sercomIndex_, pins_.sck);
 
         const bool mosiValid = ((mosiPad == 0) || (mosiPad == 3)); // MOSI must always be PAD== 0 or 3
         const bool misoValid = ((misoPad >= 0) || (misoPad <= 3)); // MISO can be on any PAD
@@ -371,7 +366,7 @@ namespace BareCpper
 
         hw_->CTRLA.reg = ctrlA;
         hw_->CTRLB.reg = ctrlB;
-
+        hw_->CTRLC.bit.DATA32B = platformConfig_.use32BitExtension;
         /// @todo Slave SPI only:  |addr = (SERCOM_SPI_ADDR_ADDR(CONF_SPIADDR) | SERCOM_SPI_ADDR_ADDRMASK(CONF_SPIADDRMASK));
         hw_->DBGCTRL.bit.DBGSTOP = true;
         // SERCOM_CRITICAL_SECTION_LEAVE();
@@ -379,13 +374,13 @@ namespace BareCpper
         return true;
       }
 
-      bool initialiseAsync(const uint8_t sercomIndex)
+      bool initialiseAsync()
       {
         // enable RXC and TXC irqs in NVIC
         // TXC is at bit pos 1 in INTFLAG, so enable SERCOMn_1 IRQ
-        const IRQn_Type txcIrqn = static_cast<IRQn_Type>(SERCOM0_1_IRQn + sercomIndex * 4);
+        const IRQn_Type txcIrqn = static_cast<IRQn_Type>(SERCOM0_1_IRQn + sercomIndex_ * 4);
         // RXC is at bit pos 2 in INTFLAG, so enable SERCOMn_2 IRQ
-        const IRQn_Type rxcIrqn = static_cast<IRQn_Type>(SERCOM0_2_IRQn + sercomIndex * 4);
+        const IRQn_Type rxcIrqn = static_cast<IRQn_Type>(SERCOM0_2_IRQn + sercomIndex_ * 4);
         NVIC_ClearPendingIRQ(txcIrqn);
         NVIC_SetPriority(txcIrqn, 1);
         NVIC_EnableIRQ(txcIrqn);
@@ -422,7 +417,7 @@ namespace BareCpper
       platformConfig_t platformConfig_;
       Pins_t pins_;
       uint8_t sercomIndex_;
-      bool transferInProgress_ = false;
+      volatile bool transferInProgress_ = false;
     };
   }
 } // END: BareCpper
